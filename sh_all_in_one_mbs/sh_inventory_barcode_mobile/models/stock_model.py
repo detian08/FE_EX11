0298ff64de6955008daa4f8b7296595513b87ecc
phoenixsampras <phoenixsampras@gmail.com>
2020-08-25 15:57:12 -0400

    QR 5.0

--- sh_all_in_one_mbs/sh_inventory_barcode_mobile/models/stock_model.py
+++ sh_all_in_one_mbs/sh_inventory_barcode_mobile/models/stock_model.py
@@ -229,14 +229,19 @@ class stock_picking(models.Model):
                 domain = [("sh_qr_code","=",self.sh_stock_barcode_mobile)]   
                                 
             elif self.env.user.company_id.sudo().sh_stock_barcode_mobile_type == 'all':
+                lot = 0
+                lote = self.env["stock.production.lot"].search([('name','=',self.sh_sale_barcode_mobile)])
+                if lote:
+                    lot = lote.product_id.id     
                 search_mls = self.move_lines.filtered(lambda ml: ml.product_id.barcode == self.sh_stock_barcode_mobile 
                                                                          or ml.product_id.default_code == self.sh_stock_barcode_mobile
                                                                          or ml.product_id.sh_qr_code == self.sh_stock_barcode_mobile
-                                                                         )
+                                                                         or ml.product_id.id == lot)
                 domain = ["|","|",
                     ("default_code","=",self.sh_stock_barcode_mobile),
                     ("barcode","=",self.sh_stock_barcode_mobile),
-                    ("sh_qr_code","=",self.sh_stock_barcode_mobile)                    
+                    ("sh_qr_code","=",self.sh_stock_barcode_mobile),
+                    ("id","=",lot),                   
                 ]  
                                                 
 
